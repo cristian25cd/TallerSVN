9daf75dcf66f3aa3f5acc444f1ae1e694c1d6772
cristian25cd <cd.copete461@uniandes.edu.co>
2014-08-12 21:38:50 -0500

    Primer commit del proyecto

---
+++ basicoSVN/src/main/webapp/src/controller/userController.js
@@ -0,0 +1,34 @@
+  App.Controller.UserController = Backbone.View.extend({
+        el: '#main',
+        initialize: function(options) {
+            this.editTemplate = _.template($('#user').html());
+            var self = this;
+            Backbone.on('user-create', function(params) {
+                self.create(params);
+            });
+            Backbone.on('user-save', function(params) {
+                self.save(params);
+            });
+            Backbone.on('user-cancel', function(params) {
+                self.cancel(params);
+            });
+        },
+        create: function() {
+            this.userModel = new App.Model.UserModel();
+            this._renderEdit();
+        },
+        save:function() { 
+            var model = $('#userForm').serializeObject();
+            var elemento=document.querySelector('#resultado');
+            var texto ='El usuario '+model.firstName+' '+model.lastName+' nació el '+model.datePicker+'.';
+            elemento.innerHTML=texto;
+        
+        },
+        cancel: function(){
+            alert('Cancel');
+        },
+        _renderEdit: function() {
+            var self = this;
+            self.$el.html(self.editTemplate({user: self.userModel}));
+        }
+    });
